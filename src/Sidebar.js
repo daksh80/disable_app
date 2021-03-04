5e2cfba3fd50ec26e4bd4ed4df18644a2740f80d
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2021-03-05 00:45:03 +0530

    css file updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,12 +2,21 @@ import { Avatar } from '@material-ui/core'
 import React from 'react'
 import "./Sidebar.css"
 function Sidebar() {
+   const recentItem = (topic) =>(
+       <div className="sidebar__recentItem">
+          <span className="sidebar__hash">#</span>
+          <p>{topic}</p>
+       </div>
+   )
     return (
         <div className="sidebar">
-           <img src="" alt=""/>
-           <Avatar />
-           <h2>Dakshendra Singh</h2>
-           <h4>pal.dakshendra80@gmail.com</h4>
+            <div className="sidebar__top">
+             <img src="https://source.unsplash.com/tD7e9hVIRZo" alt=""/>
+             <Avatar />
+             <h2>Dakshendra Singh</h2>
+             <h4>pal.dakshendra80@gmail.com</h4>
+           </div>
+
            <div className="sidebar__stats">
                <div className="sidebar__stat">
                  <p>who viewed you</p>
@@ -24,6 +33,11 @@ function Sidebar() {
            </div>
            <div className="sidebar__bottom">
                <p>Recent</p>
+               {recentItem('reactjs')}
+               {recentItem('programming')}
+               {recentItem('softwareenginnering')}
+               {recentItem('design')}
+               {recentItem('developer')}
            </div>
         </div>  
     );
