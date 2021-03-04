a4ae46e868f02f5296947bc708953bc3f00201aa
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2021-03-05 02:30:17 +0530

    firebase.js
    2:22:17

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Feed.css";
 import CreateIcon from '@material-ui/icons/Create';
 import ImageIcon from '@material-ui/icons/Image';
@@ -6,7 +6,24 @@ import InputOption from './InputOption';
 import  CalendarViewDayIcon  from '@material-ui/icons/CalendarViewDay';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import Post from './Post';
+import { db } from './firebase';
 function Feed() {
+    const [posts,setPosts]= useState([]);
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot((snapshot) =>
+            setPosts(
+                snapshot.docs.map((doc) =>({
+                    id: doc.id,
+                    data: doc.data(),
+                }))
+            )
+        );
+    }, []);
+    const sendPost = e =>{
+        e.preventDefault();
+    };
     return (
         <div className='feed'>
             <div className="feed__inputContainer">
@@ -14,7 +31,7 @@ function Feed() {
                    <CreateIcon />   
                    <form>
                        <input type="text"/>
-                       <button type ='submit' >Send</button>
+                       <button onClick={sendPost} type ='submit' >Send</button>
                    </form>
                 </div> 
                 <div className="feed__inputOptions">
@@ -24,8 +41,15 @@ function Feed() {
                     <InputOption Icon={CalendarViewDayIcon} title='Write artical' color='#7FC15E'/>
                 </div>
             </div>
+
+            {/* posts */}
+            {posts.map((post)=>(
+                <Post />
+            ))}
+            <Post name='Dakshendra Singh' description='This is a test'
+            message='Wow this worked'/>
         </div>
-    )
+    );
 }
 
 export default Feed;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebaseapp from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC-vUzPTXO2wYrHBWFseFnW2_QvKzdPOOw",
+    authDomain: "disable-app-35755.firebaseapp.com",
+    projectId: "disable-app-35755",
+    storageBucket: "disable-app-35755.appspot.com",
+    messagingSenderId: "449073384156",
+    appId: "1:449073384156:web:3212bf1e1d49d2e17e0219"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db , auth};
