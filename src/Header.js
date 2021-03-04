c3408a7d63517ea0e13c91999976a0e2c2b628bf
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2021-03-04 16:47:31 +0530

    Updated file
    insert images, created Header.css,file

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import Logo from './Logo.png'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+
 function Header() {
     return (
         <div className="header">
-            <h1>this is header</h1>
+            
             
             <div className="header_left">
-                <img src="" alt=""/>
+                <img src={Logo} alt=""/>
 
                 <div className="header_search">
-                    {/* Search Icon*/}
+                     <SearchIcon />
                     <input type="text"/>
                 </div>
             </div>
 
             <div className="header_right">
-
+                <HeaderOption Icon={HomeIcon} title='Home'/>
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
             </div>
             
         </div>
